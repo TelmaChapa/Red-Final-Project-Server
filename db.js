0b3e12292730e43491a2efd32834b2b3a4222319
Telma Chapa <telchapa@gmail.com>
2021-11-16 22:33:47 -0500

    final push

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  dialectOptions: !process.env.DATABASE_URL.includes("localhost")
-    ? {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // fixing unhandled rejection
-      },
-    }
-    : {},
+  // dialectOptions: !process.env.DATABASE_URL.includes("localhost")
+  //   ? {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false, // fixing unhandled rejection
+  //     },
+  //   }
+  //   : {},
 });
 
 module.exports = db;
